3a2849a31683eb0554c4eaf855bc0e4cc1621ca3
NathalieBarbosa <n.barbosa@getaclub.io>
2020-11-07 17:11:52 -0500

    Bubble shader V1

---
+++ Assets/Scripts/testN/BubbleController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleController : MonoBehaviour
+{
+    private Material m_material;
+    private GameObject m_playerPosition;
+    private Vector3 m_oldPlayerPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_material = GetComponent<Renderer>().material;
+        m_playerPosition = GameObject.Find("Player");
+        SetPlayerPosition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_playerPosition.transform.position != m_oldPlayerPosition)
+        {
+            SetPlayerPosition();
+        }
+    }
+    void SetPlayerPosition()
+    {
+        if (m_playerPosition != null)
+        {
+            m_material.SetVector("_PlayerPos", m_playerPosition.transform.position);
+            m_oldPlayerPosition = m_playerPosition.transform.position;
+        }
+    }
+
+}
---
+++ Assets/Scripts/testN/DistancePos.hlsl
@@ -0,0 +1,15 @@
+﻿void DistancePos_half(in float3 playerPos, in float3 worldPos, in float radius, in float4 primaryTexture, in float4 secondaryTexture, out float4 Out )
+{
+	if (distance(playerPos.xyz, worldPos.xyz) > radius)
+	{
+		Out = primaryTexture;
+	}
+	else if (distance(playerPos.xyz, worldPos.xyz) > radius -0.03)
+	{ 
+		Out = float4 (1,1,1,1);
+	}
+	else
+	{
+		Out = secondaryTexture;
+	}
+}
