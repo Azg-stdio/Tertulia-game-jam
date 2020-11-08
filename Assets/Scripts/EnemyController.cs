37791227efcae5d0c90ab0eef759a320d927b757
Nicasanovac <nicasanovac@gmail.com>
2020-11-07 23:24:50 -0500

    Enemy FSM based on animator, first iteration

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+    private NavMeshAgent agent;
+    public bool isChasing { get; set; }
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (isChasing)
+        {
+            agent.destination = player.transform.position;
+            agent.isStopped = false;
+        }
+        else
+        {
+            agent.isStopped = true;
+        }
+    }
+    void MoveToOnClickedPoint()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                agent.destination = hit.point;
+            }
+        }
+    }
+}
