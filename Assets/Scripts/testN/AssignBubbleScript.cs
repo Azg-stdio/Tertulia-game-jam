03a5997b0afe4daf07b2f85261ffa7c500305d67
NathalieBarbosa <n.barbosa@getaclub.io>
2020-11-08 11:12:05 -0500

    Variation and assets

---
+++ Assets/Scripts/testN/AssignBubbleScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class AssignBubbleScript : MonoBehaviour
+{
+    [SerializeField] GameObject[] interactables;
+    void Start()
+    {
+        interactables = GameObject.FindGameObjectsWithTag("InteractableBubble") ;
+        for (int i = 0; i < interactables.Length; i++)
+        {
+
+            interactables[i].AddComponent<BubbleController>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
