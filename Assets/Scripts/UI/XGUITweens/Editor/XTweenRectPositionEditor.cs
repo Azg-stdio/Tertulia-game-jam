67499719e56972b490916f5b0d9e10d9f717153a
Andre Martinez <artcadev@gmail.com>
2020-11-09 22:42:53 -0300

    [Update] XTween and VFXs

---
+++ Assets/Scripts/UI/XGUITweens/Editor/XTweenRectPositionEditor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+[CanEditMultipleObjects]
+[CustomEditor(typeof(XTweenRectPosition))]
+public class XTweenRectPositionEditor : Editor 
+{
+	/// <summary>
+	/// Changes the inspector
+	/// </summary>
+	public override void OnInspectorGUI ()
+	{
+		XTweenRectPosition myTarget = (XTweenRectPosition)target;
+		
+		myTarget.minFrom = EditorGUILayout.Vector2Field("Min From", myTarget.minFrom);
+		myTarget.maxFrom = EditorGUILayout.Vector2Field("Max From", myTarget.maxFrom);
+		myTarget.minTo = EditorGUILayout.Vector2Field("Min To", myTarget.minTo);
+		myTarget.maxTo = EditorGUILayout.Vector2Field("Max To", myTarget.maxTo);
+		
+		DrawTweener(myTarget);
+	}
+
+	/// <summary>
+	/// Tweener values that belong in the inspector
+	/// </summary>
+	public void DrawTweener(XTweenRectPosition myTarget)
+	{
+		myTarget.playStyle =(XTweener.Style)EditorGUILayout.EnumPopup("Play Style", myTarget.playStyle);
+		myTarget.animationCurve = 			EditorGUILayout.CurveField("Animationcurve", myTarget.animationCurve);
+		myTarget.duration = 				EditorGUILayout.FloatField("Duration", myTarget.duration);
+		myTarget.startDelay = 				EditorGUILayout.FloatField("Start Delay", myTarget.startDelay);
+		myTarget.ignoreTimescale = 			EditorGUILayout.Toggle("Ignore Timescale", myTarget.ignoreTimescale);
+	}
+}
---
+++ Assets/Scripts/UI/XGUITweens/Editor/XTweenTextEditor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+[CanEditMultipleObjects]
+[CustomEditor(typeof(XTweenText))]
+public class XTweenTextEditor : Editor {
+    /// <summary>
+    /// Changes the inspector
+    /// </summary>
+    public override void OnInspectorGUI() {
+        XTweenText myTarget = (XTweenText)target;
+
+        var serializedObject = new SerializedObject(myTarget);
+        var property = serializedObject.FindProperty("values");
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(property, true);
+        serializedObject.ApplyModifiedProperties();
+
+        DrawTweener(myTarget);
+    }
+
+    /// <summary>
+    /// Tweener values that belong in the inspector
+    /// </summary>
+    public void DrawTweener(XTweenText myTarget) {
+        myTarget.playStyle = (XTweener.Style)EditorGUILayout.EnumPopup("Play Style", myTarget.playStyle);
+        myTarget.animationCurve = EditorGUILayout.CurveField("Animationcurve", myTarget.animationCurve);
+        myTarget.duration = EditorGUILayout.FloatField("Duration", myTarget.duration);
+        myTarget.startDelay = EditorGUILayout.FloatField("Start Delay", myTarget.startDelay);
+        myTarget.ignoreTimescale = EditorGUILayout.Toggle("Ignore Timescale", myTarget.ignoreTimescale);
+    }
+}
