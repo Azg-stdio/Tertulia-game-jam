1da83545f1c228334cbb3894896b64bf7f014c35
Nicasanovac <nicasanovac@gmail.com>
2020-11-09 19:35:14 -0500

    Player now changes Hero Animator

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,15 +6,25 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 10.0f;
     [SerializeField] private float runSpeed = 20.0f;
+
+    [SerializeField] Animator animator;
+    private string currentState;
     private Rigidbody rb;
     private Vector3 direction;
     private bool isRunning = false;
     private bool isMoving = true;
     public GameObject canvas;
 
+    //Animation names
+    const string WALK_FRONT = "HeroWalkFrontViewWander";
+    const string WALK_BACK = "HeroWalkBackwards";
+    const string STAND_FRONT = "HeroIdleWander";
+    const string STAND_BACK = "HeroIdleStandBack";
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        ChangeAnimationState(STAND_FRONT);
     }
 
     // Update is called once per frame
@@ -24,7 +34,13 @@ public class PlayerController : MonoBehaviour
             direction.x = Input.GetAxis("Horizontal");
             direction.z = Input.GetAxis("Vertical");
             direction.Normalize();
-            isRunning =  Input.GetButton("Run");
+            if(direction.z > 0.05f)ChangeAnimationState(WALK_BACK);
+            else if(direction.z < -0.05f || direction.x != 0.0f)ChangeAnimationState(WALK_FRONT);
+            else {
+                ChangeAnimationState(STAND_FRONT);
+                //ChangeAnimationState(STAND_BACK);
+            }
+            //isRunning =  Input.GetButton("Run");
         }   
         if (Input.GetKeyDown("space")){     
             if (canvas.activeSelf){
@@ -40,12 +56,19 @@ public class PlayerController : MonoBehaviour
         }        
     }
 
+    void ChangeAnimationState(string newState){
+        if(currentState == newState) return;
+        animator.Play(newState);
+        currentState = newState;
+    }
+
     public void StartMoving(){
         isMoving=true;
     }
 
     public void StopMoving(){
         isMoving=false;
+        ChangeAnimationState(STAND_FRONT);
         rb.velocity=Vector3.zero;
     }
 }
