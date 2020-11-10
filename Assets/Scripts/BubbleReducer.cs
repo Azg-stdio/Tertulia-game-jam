c6b9cf50847185eea8b4a5b3134b3c8fe7298ee4
miguelarbelaez <azg.stdio@gmail.com>
2020-11-09 23:11:14 -0500

    Push Final v2 (?)

--- Assets/Scripts/BubbleReducer.cs
+++ Assets/Scripts/BubbleReducer.cs
@@ -7,12 +7,19 @@ public class BubbleReducer : MonoBehaviour
 // Array to store all the GameObjects in the scene
     GameObject[] AllGameObjects;
     public float bubbleradius=5.0f;
+    public int pieces=0;
     public GameObject bubble;
+    public GameObject door;
     public void ReduceBubble () {
         bubbleradius-=0.5f;    
         bubble.transform.localScale*=0.86f;  
         ChangeMaterials();
+        pieces++;
+        if(pieces >= 6){
+            door.GetComponent<SphereCollider>().enabled=true;
+        }
     }
+
     void ChangeMaterials(){
         AllGameObjects = GameObject.FindObjectsOfType(typeof(GameObject)) as GameObject[];
         foreach (GameObject go in AllGameObjects) {
